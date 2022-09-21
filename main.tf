1b402da4c2f9a8e2b7f13e94b046ea0e6ae9e7e2
Vladyslav Vlasenko <fl4shqq@gmail.com>
2022-09-21 14:56:12 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,8 @@ resource "random_pet" "pet" {
     # Generate a new pet name each time
     name = "Vlad"
   }
-}
 
-output {
+output "outt" {
   value = "Lorem dfipsusdm dolor sit ameывt, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
 }
 
